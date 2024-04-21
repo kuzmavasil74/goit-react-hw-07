550c9fd239d30aefc8b3f916e013b035fc1823c9
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-04-21 20:30:39 +0200

    move_App_to_components(folder)

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,12 @@
-import ContactForm from './components/ContactForm/ContactForm'
-import ContactList from './components/ContactList/ContactList'
-import SearchBox from './components/SearchBox/SearchBox'
+import ContactForm from '../ContactForm/ContactForm'
+import ContactList from '../ContactList/ContactList'
+import SearchBox from '../SearchBox/SearchBox'
 
 import css from './App.module.css'
 import { useDispatch } from 'react-redux'
 
 import { useEffect } from 'react'
-import { fetchContacts } from './redux/contactsOps'
+import { fetchContacts } from '../../redux/contactsOps'
 
 function App() {
   const dispatch = useDispatch()
